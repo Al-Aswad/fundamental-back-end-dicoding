5f9b9b73d184c07e749612279b0116fd12acd1da
Hajar Aswad <hajaraswadkom@gmail.com>
2022-09-13 22:52:06 +0700

    authorization

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -4,21 +4,24 @@ const { nanoid } = require('nanoid');
 const InvariantError = require('../../exeptions/InvariantError');
 const { mapDBToModel } = require('../../utils');
 const NotFoundError = require('../../exeptions/NotFoundError');
+const AuthorizationError = require('../../exeptions/AuthorizationError');
 
 class NotesService {
     constructor() {
         this._pool = new Pool();
     }
 
-    async addNote({ title, body, tags }) {
+    async addNote({
+        title, body, tags, owner,
+    }) {
         const id = nanoid(16);
 
         const createdAt = new Date().toISOString();
         const updatedAt = createdAt;
 
         const query = {
-            text: 'INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
-            values: [id, title, body, tags, createdAt, updatedAt],
+            text: 'INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id',
+            values: [id, title, body, tags, createdAt, updatedAt, owner],
         };
 
         const result = await this._pool.query(query);
@@ -30,8 +33,8 @@ class NotesService {
         return result.rows[0].id;
     }
 
-    async getNotes() {
-        const result = await this._pool.query('SELECT * FROM notes');
+    async getNotes(owner) {
+        const result = await this._pool.query('SELECT * FROM notes WHERE owner = $1', [owner]);
         return result.rows.map(mapDBToModel);
     }
 
@@ -64,19 +67,6 @@ class NotesService {
         }
     }
 
-    // async deleteNoteById(id) {
-    //     const query = {
-    //         text: 'DELETE FROM notes WHERE id = $1 RETURNING id',
-    //         values: [id],
-    //     };
-
-    //     const result = await this._pool.query(query);
-
-    //     if (!result.rows.length) {
-    //         throw new NotFoundError('Catatan gagal dihapus. Id tidak ditemukan');
-    //     }
-    // }
-
     async deleteNoteById(id) {
         const query = {
             text: 'DELETE FROM notes WHERE id = $1 RETURNING id',
@@ -87,6 +77,25 @@ class NotesService {
 
         if (!result.rows.length) {
             throw new NotFoundError('Catatan gagal dihapus. Id tidak ditemukan');
+        }
+    }
+
+    async verifyNoteOwner(id, owner) {
+        const query = {
+            text: 'SELECT * FROM notes WHERE id = $1',
+            values: [id],
+        };
+
+        const result = await this._pool.query(query);
+
+        if (!result.rows.length) {
+            throw new NotFoundError('Catatan tidak ditemukan');
+        }
+
+        const note = result.rows[0];
+
+        if (note.owner !== owner) {
+            throw new AuthorizationError('Anda tidak berhak mengakses resource ini');
         }
     }
 }
